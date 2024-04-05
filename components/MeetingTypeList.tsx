ed80fcb15e9b4c7790381034a7c2c8a9a96a7663
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-04-05 11:53:21 +0530

    toast add

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import MeetingModel from "./MeetingModel";
 import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "./ui/use-toast";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -21,11 +22,19 @@ const MeetingTypeList = () => {
     link: "",
   });
   const [callDetails, setCallDetails] = useState<Call>();
+  const { toast } = useToast();
 
   const createMeeting = async () => {
     if (!client || !user) return;
 
     try {
+      if (!values.dateTime) {
+        toast({
+          title: "Please select a date and time",
+        });
+        return;
+      }
+
       const id = crypto.randomUUID();
       const call = client.call("default", id);
 
@@ -49,8 +58,15 @@ const MeetingTypeList = () => {
       if (!values.description) {
         router.push(`/meeting/${call.id}`);
       }
+
+      toast({
+        title: "Meeting Create",
+      });
     } catch (error) {
       console.log(error);
+      toast({
+        title: "Failed to create meeting",
+      });
     }
   };
 
